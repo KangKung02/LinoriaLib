0be55c590612fb901fad43873f5ad705c6f7fff2
KangKung <87695599+KangKung02@users.noreply.github.com>
2022-03-09 16:41:31 +0700

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -140,7 +140,7 @@ local SaveManager = {} do
     function SaveManager:AutoSave()
         coroutine.wrap(function()
             while true do
-		if Toggles["Auto Save"] and Toggles["Auto Save"] then
+		if Toggles["Auto Save"] and Toggles["Auto Save"].Value then
                     self:Save();
                     task.wait(1);
 		end
